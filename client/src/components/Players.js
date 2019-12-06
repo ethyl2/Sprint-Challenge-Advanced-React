2064ed027eec4f1bdb5a6d139647bfd17aef036b
Heather Nuffer <zzzuzz@gmail.com>
2019-12-06 11:17:26 -0700

    Added recharts library

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -11,7 +11,7 @@ class Players extends Component {
     componentDidMount() {
         Axios.get('http://localhost:5000/api/players')
         .then(response => {
-            //console.log(response.data);
+            console.log(response.data);
             this.setState({players: response.data});
         })
         .catch(err => console.log(err));
