ed0e516dc25a7a9e6deba6c8728a1c5d785620ef
Heather Nuffer <zzzuzz@gmail.com>
2019-12-06 11:32:16 -0700

    Stretch: added graph using recharts library to display data from server

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import trophy from '../images/trophy.svg';
 import Axios from 'axios';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 class Players extends Component {
     constructor() {
@@ -11,7 +12,7 @@ class Players extends Component {
     componentDidMount() {
         Axios.get('http://localhost:5000/api/players')
         .then(response => {
-            console.log(response.data);
+            //console.log(response.data);
             this.setState({players: response.data});
         })
         .catch(err => console.log(err));
@@ -22,10 +23,16 @@ class Players extends Component {
             return <h2>Loading...</h2>
         } else {
             return (<div>
-                    <h1>Players</h1>
-                    <img src={trophy} alt='trophy' className='trophy'/>
-                    {this.state.players.map(player => <h2 key={player.id}>{player.name} from {player.country}</h2>)}
-                </div>)
+                        <h1>Players</h1>
+                        <img src={trophy} alt='trophy' className='trophy'/>
+                        {this.state.players.map(player => <h2 key={player.id}>{player.name} from {player.country}</h2>)}
+                        <div className='graphContainer'>
+                            <LineChart width={800} height={400} data={this.state.players}>
+                                <Line type="monotone" dataKey="searches" stroke="#8884d8" />
+                                <CartesianGrid stroke="#ccc" />
+                            </LineChart>
+                        </div>
+                    </div>)
         }
     }
 } 
